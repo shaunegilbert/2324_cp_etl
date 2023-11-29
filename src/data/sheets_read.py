9cae69edcf94d5e7cf5cca2b3ffdb510c2f48630
shaunegilbert <shaune.gilbert@readyct.org>
2023-11-28 20:54:05 -0500

    sa read working

--- src/data/sheets_read.py
+++ src/data/sheets_read.py
@@ -1,27 +1,16 @@
 # sa_read.py
 import os
 from dotenv import load_dotenv
-from src.utils.sheets_api_util import get_google_sheets_service
+from src.utils.sheets_api_util import (get_google_sheets_service,
+                                       read_sheet, 
+                                       save_sheet_as_csv)
 
-def read_sheet(spreadsheet_id, range_name, service):
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
-    values = result.get('values', [])
-    return values
-
-def download_sheet(spreadsheet_id, range_name):
-    load_dotenv()
-    service = get_google_sheets_service()
-    data = read_sheet(spreadsheet_id, range_name, service)
-    if not data:
-        print('No data found.')
-    else:
-        # You can either return this data or save it to a file
-        return data
 
 def download_sa():
+    service=get_google_sheets_service()
     SPREADSHEET_ID = os.getenv('SA_GS_ID')
-    RANGE_NAME = 'sa_query!A1:AB1'
-    sheet_data = download_sheet(SPREADSHEET_ID, RANGE_NAME)
-    print(sheet_data)
-
+    RANGE_NAME = os.getenv('SA_GS_RANGE')
+    sheet_data = read_sheet(SPREADSHEET_ID, RANGE_NAME, service)
+    
+    if sheet_data:
+        save_sheet_as_csv(sheet_data, 'student_agreement.csv')
--- src/utils/sheets_api_util.py
+++ src/utils/sheets_api_util.py
@@ -2,6 +2,7 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import tempfile
 import os
+import csv
 from dotenv import load_dotenv
 
 from src.utils.get_param import get_parameter
@@ -21,12 +22,39 @@ def get_google_sheets_service():
     return service
 
 def read_sheet(spreadsheet_id, range_name, service):
+    # Access the Google Sheets API
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
     values = result.get('values', [])
 
+    # Check if data was found
     if not values:
         print('No data found.')
+        return None  # Return None to indicate no data was found
     else:
+        # Print each row of the data
         for row in values:
-            print(row)
+            return values
+        # Return the data for further processing if needed
+        
+    
+def save_sheet_as_csv(data, filename):
+    raw_data_dir = os.path.expanduser(os.getenv('RAW_DATA_DIR'))
+    filepath = os.path.join(raw_data_dir, filename)
+    
+    # Write data to a CSV file
+    with open(filepath, 'w', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(data)
+
+    print(f"Data saved as {filepath}")
+
+# def download_sheet(spreadsheet_id, range_name):
+#     load_dotenv()
+#     service = get_google_sheets_service()
+#     data = read_sheet(spreadsheet_id, range_name, service)
+#     if not data:
+#         print('No data found.')
+#     else:
+#         # You can either return this data or save it to a file
+#         return data
