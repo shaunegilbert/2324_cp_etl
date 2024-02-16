5f8912a22a63204dd0ee9667487fc73f57d15311
shaunegilbert <shaune.gilbert@readyct.org>
2024-02-16 13:01:41 -0500

    update main.py to reenable emails

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ from src.features import c3_wrangle
 from src.utils.send_email import send_email
 
 def main ():
-    # try:
+    try:
         sftp_read.main()
         sheets_read.main()
         c3_wrangle.main()
         sheets_write.main()
         write_to_s3.main()
         
-    #     # Send a success email
-    #     send_email('shaune.gilbert@readyct.org', 'CP ETL Script Execution Successful', 'The CP ETL script ran successfully.')
+        # Send a success email
+        send_email('shaune.gilbert@readyct.org', 'CP ETL Script Execution Successful', 'The CP ETL script ran successfully.')
 
         
         
-    # except Exception as e:
-    #     logging.error(f"Script failed to run. Error: {str(e)}")
+    except Exception as e:
+        logging.error(f"Script failed to run. Error: {str(e)}")
 
-    #     # Send an error email
-    #     send_email('shaune.gilbert@readyct.org', 'CP ETL Script Execution Failed', f"The CP ETL script failed to run. Error: {str(e)}")
+        # Send an error email
+        send_email('shaune.gilbert@readyct.org', 'CP ETL Script Execution Failed', f"The CP ETL script failed to run. Error: {str(e)}")
         
         
 if __name__ == "__main__":
