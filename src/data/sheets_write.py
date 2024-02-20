3dd5026cbc8f83e843ce93987a4983803df56351
shaunegilbert <shaune.gilbert@readyct.org>
2024-02-20 13:18:18 -0500

    update hps student push to sheets

--- src/data/sheets_write.py
+++ src/data/sheets_write.py
@@ -11,10 +11,20 @@ def processed_c3():
     csv_file=os.path.join('data', 'processed', 'c3_processed.csv')
     
     clear_and_write_csv_to_sheet(spreadsheet_id, range_name, csv_file, service)
+    
+def hps_sftp_push():
+    service = get_google_sheets_service()
+    spreadsheet_id = os.getenv('HPS_STUDENTS_ID')
+    range_name = os.getenv('HPS_STUDENTS_RANGE')
+    csv_file=os.path.join('data', 'raw', 'students.csv')
+    
+    clear_and_write_csv_to_sheet(spreadsheet_id, range_name, csv_file, service)
+
 
 def main():
     # Call your functions here
     processed_c3()
+    hps_sftp_push()
 
 if __name__ == "__main__":
     main()
