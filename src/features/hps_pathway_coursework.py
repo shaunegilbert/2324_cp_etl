8ee3ee4bb8fa22490e67eab3a35ac34583152d05
shaunegilbert <shaune.gilbert@readyct.org>
2024-03-08 13:42:32 -0500

    final course pivot completed

--- src/features/hps_pathway_coursework.py
+++ src/features/hps_pathway_coursework.py
@@ -61,7 +61,59 @@ def pull_current_courses (base_dirs):
     file_path = os.path.join(base_dirs['raw'], 'current_courses.csv')
     columns = ['STUDENT_NUMBER',
                'COURSE_NUMBER']
+    df=read_csv_column(file_path, columns)
 
+    # Add a new column 'currently_enrolled' with value 'enrolled'
+    df['status'] = 'enrolled'
+
+    output_file_path = os.path.join(base_dirs['interim'], 'current_courses_interim.csv')
+    df.to_csv(output_file_path, index=False)
+    logging.info(f"WBL DataFrame has been written to {output_file_path}")
+    return df
+
+def pull_completed_courses (base_dirs):
+    file_path = os.path.join(base_dirs['raw'], 'stored_grades.csv')
+    columns=['STUDENT_NUMBER',
+             'COURSE_NUMBER',
+             'EARNEDCRHRS']
+    df=read_csv_column(file_path, columns)
+
+    # Add a new column 'status' based on the condition: if EARNEDCRHRS == 1 then 'passed', else 'na'
+    df['status'] = df['EARNEDCRHRS'].apply(lambda x: 'passed' if x == 1 else 'no credit earned')
+
+    # Drop the 'EARNEDCRHRS' column
+    df.drop(columns=['EARNEDCRHRS'], inplace=True)
+
+    output_file_path = os.path.join(base_dirs['interim'], 'completed_courses_interim.csv')
+    df.to_csv(output_file_path, index=False)
+    logging.info(f"WBL DataFrame has been written to {output_file_path}")
+    return df
+
+def final_course_df (base_dirs):
+    df_current = pull_current_courses(base_dirs)
+    df_completed = pull_completed_courses(base_dirs)
+    df_codes=pull_course_codes(base_dirs)
+    students=pull_students_sub(base_dirs)
+
+    # Merge the two DataFrames vertically
+    df_merged = pd.concat([df_current, df_completed])
+    
+    # Merge df_merged with df_codes on 'COURSE_NUMBER' and 'course_code_l'
+    df_merged = pd.merge(df_merged, df_codes, how='left', left_on='COURSE_NUMBER', right_on='course_code_l')
+
+    #drop course code columns
+    df_merged.drop(columns=['COURSE_NUMBER',
+                     'course_code_l'], inplace=True)
+    
+     # Pivot the DataFrame
+    df_pivot = df_merged.pivot_table(index='STUDENT_NUMBER', columns='course_name_l', values='status', aggfunc='first')
+
+    df_pivot = pd.merge(students, df_pivot, how='left', on='STUDENT_NUMBER')
+
+    output_file_path = os.path.join(base_dirs['interim'], 'final_course_pivot.csv')
+    df_pivot.to_csv(output_file_path, index=False)
+    logging.info(f"Merged DataFrame has been written to {output_file_path}")
+    return df_merged
 
 def main():
     try:
@@ -74,6 +126,9 @@ def main():
         #process individual datasets
         pull_students_sub (base_dirs)
         pull_course_codes (base_dirs)
+        pull_current_courses (base_dirs)
+        pull_completed_courses(base_dirs)
+        final_course_df(base_dirs)
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
